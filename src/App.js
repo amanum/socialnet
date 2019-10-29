f76068677a88c955c07094988ce49769aaa758f2
Amanzhol Umurzakov <umurzakov.amanzhol@gmail.com>
2019-10-29 15:08:47 +0600

    'lesson61'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import DialogsPage from "./components/DialogsPage/DialogsPage";
 import {Route} from "react-router-dom";
 import UsersPageContainer from "./components/UsersPage/UsersPageContainer";
 import ProfilePageContainer from "./components/ProfilePage/ProfilePageContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <HeaderContainer/>
             <Sidebar/>
             <Route path="/profile/:userId?" render={() => <ProfilePageContainer/>}/>
             <Route path="/dialogs" render={() => <DialogsPage/>}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-	return (
-		<header className={s.AppHeader}>
-		  <div>
-			  <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt=""/>
-		  </div>
-		</header>
-	);
+const Header = (props) => {
+    return (
+        <header className={s.AppHeader}>
+            <div>
+                <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt=""/>
+            </div>
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login
+                    : <NavLink to={'/login'}>LogIn</NavLink>
+                }
+            </div>
+        </header>
+    );
 }
 
 export default Header;
