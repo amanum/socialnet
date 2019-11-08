5e3ce9847ef39c3e7193bd288e677d7d1a398680
Amanzhol Umurzakov <umurzakov.amanzhol@gmail.com>
2019-11-08 16:08:09 +0600

    Updates

---
+++ precache-manifest.c9b318f7a42f9e27ffe1222e076ee105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c63cc1e792dc047ddfa104316bbe92",
+    "url": "/socialnet/index.html"
+  },
+  {
+    "revision": "6bc571dee56bc92809a9",
+    "url": "/socialnet/static/css/main.e7a1bda5.chunk.css"
+  },
+  {
+    "revision": "842976b23e4afbed7da8",
+    "url": "/socialnet/static/js/2.f2c5b37e.chunk.js"
+  },
+  {
+    "revision": "6bc571dee56bc92809a9",
+    "url": "/socialnet/static/js/main.4bf57210.chunk.js"
+  },
+  {
+    "revision": "5e20f6257b01846a212f",
+    "url": "/socialnet/static/js/runtime-main.8a1de007.js"
+  },
+  {
+    "revision": "e8272eca3ae9dd6ab78eaf7165701e16",
+    "url": "/socialnet/static/media/Ripple-1s-200px.e8272eca.svg"
+  }
+]);
